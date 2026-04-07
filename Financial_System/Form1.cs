c4627111dba9beb298442f9fa09a44f83dbcbe54
mohamed <mohamedashraf3758@gmail.com>
2026-04-07 10:27:50 +0200

    .

--- Financial_System/Form1.cs
+++ Financial_System/Form1.cs
@@ -9,8 +9,7 @@ namespace Financial_System
 
         private void btn1_Click(object sender, EventArgs e)
         {
-            string name = " 😘😘 خد رقمي يا حبي 01021799781";
-            lbl1.Text = name;
+            
         }
     }
 }
