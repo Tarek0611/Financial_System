3157363671d0ef22bf347e468abe684cd2b6bebe
mohamed <mohamedashraf3758@gmail.com>
2026-04-07 10:39:00 +0200

    .

--- Financial_System/Properties/Resources.Designer.cs
+++ Financial_System/Properties/Resources.Designer.cs
@@ -69,15 +69,5 @@ namespace Financial_System.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Screenshot_2026_04_07_083915 {
-            get {
-                object obj = ResourceManager.GetObject("Screenshot 2026-04-07 083915", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
